c803db323c651dec2a8ef67d2b870c2480b60761
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2024-10-27 02:53:12 +0100

    initialized sample project for multiple test func.

---
+++ src/main/java/com/example/testmultiple/TestmultipleApplication.java
@@ -0,0 +1,15 @@
+package com.example.testmultiple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmultipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmultipleApplication.class, args);
+
+		System.out.println("Sample test");
+	}
+
+}
---
+++ src/test/java/com/example/testmultiple/TestmultipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmultiple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmultipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
